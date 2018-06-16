36ab89d6900c0d7923d320233bb68d8508de7158
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-16 18:04:39 +0100

    add all and delete methods

--- models/show.rb
+++ models/show.rb
@@ -3,7 +3,7 @@ require_relative('../db/sql_runner.rb')
 class Show
 
   attr_reader :id, :name, :type
-  
+
   def initialize(options)
     @id = options['id'].to_i if options ['id']
     @name = options['name']
@@ -25,7 +25,19 @@ class Show
 
   def self.delete_all()
     sql = "DELETE FROM shows"
-    SqlRunner.run( sql )
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM shows"
+    show_data = SqlRunner.run(sql)
+    return show_data.map { |show| Show.new(show) }
+  end
+
+  def delete(id)
+    sql = "DELETE FROM shows WHERE id = $1"
+    values = [id]
+    SqlRunner.run(sql, values)
   end
 
 end
